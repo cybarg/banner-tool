5351d9c8d46585fb9195c639354d2b45b767d379
cybarg <cb@prettylogic.com>
2012-10-21 01:11:52 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BannerTool::Application.config.secret_token = '839de99933aea15de6756d9e44e87e2d7b2e146d1edc4df4436b7aa36c84add1fd0cb6bffe4ab01b0a9a2d56baa6b7713c8ea1e2e97af9dbef626557f9440160'
